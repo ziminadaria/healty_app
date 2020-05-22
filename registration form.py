42b9be89a75de940931db7333a66722b1498a828
ziminadaria <65713539+ziminadaria@users.noreply.github.com>
2020-05-22 13:35:11 +0300

    Add files via upload
    version 4

--- registration form.py
+++ registration form.py
@@ -2,49 +2,46 @@ def start_of_app():
     print("Welcome to the Healthy lifestyle application!")
     existing_account = input("Do you have an account? (yes/no): ")
     if existing_account == 'yes':
-        name = input("Please, enter your name: ")
+        name = input("Please, enter your username: ")
         main_menu(main_menu1)
     elif existing_account == 'no':
         add_to_data_base(registration_form())
         print('Choose your goal: ')
         main_menu(goal)
 
+
 def take_data_from_database():
     data_base = {}
-    with open("data base.txt",'r') as file:
+    with open("data base.txt", 'r') as file:
         for line in file:
             name = line.split(',')[0]
             sex = line.split(',')[1]
             age = line.split(',')[2]
             weight = line.split(',')[3]
             height = line.split(',')[4]
-            lifestyle= line.split(',')[5]
-            personal_data = dict( name = name, sex = sex, age = age, weight = weight,height = height,lifestyle = lifestyle)
+            lifestyle = line.split(',')[5]
+            personal_data = dict(name=name, sex=sex, age=age, weight=weight, height=height, lifestyle=lifestyle)
             data_base[name] = personal_data
     return data_base
 
+
 def registration_form():
     print(" Please, fill in the registration form: ")
-    name = input("Enter your name: ")
+    name = input("Enter your username: ")
     sex = input("Enter your sex (male/female): ")
     age = int(input("Enter your full age: "))
     weight = int(input("Enter your weight: "))
     height = int(input("Enter your height: "))
     lifestyle = input("Enter your type of lifestyle (active/not active): ")
-    data = {}
-    data["name"] = name
-    data["sex"] = sex
-    data["age"] = age
-    data["weight"] = weight
-    data["height"] = height
-    data["lifestyle"] = lifestyle
+    data = {"username": name, "sex": sex, "age": age, "weight": weight, "height": height, "lifestyle": lifestyle}
     return data
 
+
 def add_to_data_base(data):
-    with open("data base.txt",'a') as file:
+    with open("data base.txt", 'a') as file:
         file.write("\n")
         for key in data:
-               file.write(str(data[key])+",")
+            file.write(str(data[key]) + ",")
 
 
 def main_menu(dictionary):
@@ -61,8 +58,10 @@ def main_menu(dictionary):
 def my_profile():
     pass
 
+
 def my_diet():
     pass
+
 
 def my_training_plan():
     pass
@@ -75,8 +74,10 @@ def exit_programme():
 def lose_weight():
     main_menu(main_menu1)
 
+
 def gain_weight():
     main_menu(main_menu1)
+
 
 def keep_in_shape():
     main_menu(main_menu1)
@@ -86,11 +87,11 @@ main_menu1 = {'1': ("1)See my profile", my_profile),
               '2': ("2)See my diet", my_diet),
               '3': ("3)See my training plan", my_training_plan),
               '4': ("4)Exit", exit_programme)
-             }
+              }
 
 goal = {'1': ("1)To lose weight", lose_weight),
         '2': ("2)To gain weight", gain_weight),
         '3': ("3)To keep in shape", keep_in_shape)
-       }
+        }
 
 start_of_app()
